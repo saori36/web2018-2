6d05ed409f618a116967385f4dd0bfb49d20dea6
zm17454 <zm17454@josai.ac.jp>
2018-10-14 21:55:51 -0400

    messaage

---
+++ ex06-6.js
@@ -0,0 +1,13 @@
+var who65 = 'world';
+function sayhello66(){
+ alert('Hello,'+ who66 + '!');
+}
+function taro(){
+ who66 ='太郎が来た';
+ alert('太郎が来た'+'!');
+}
+function hanako(){
+ who66 ='花子が来た';
+  alert('花子が来た' +'!');
+
+}
---
+++ ex06-8.js
@@ -0,0 +1,10 @@
+function sayhello68(){
+  alert('Hello,'+document.getElementByld('who68').innerHTML+
+  '!');
+}
+function taro68(){
+ document.getElementByid('who68').innerHTML ='太郎';
+}
+function hanako68(){
+  document.getElementByid('who68').innerHTML ='花子';
+}
